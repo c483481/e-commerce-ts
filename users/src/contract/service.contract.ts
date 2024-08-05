9064042e611e077415ffe7f6a56989dcce02806f
c483481 <charleskojansow@gmail.com>
2024-08-05 21:55:44 +0800

    add update profile on service

--- users/src/contract/service.contract.ts
+++ users/src/contract/service.contract.ts
@@ -1,6 +1,6 @@
 import { UserSession } from "../module/dto.module";
 import { AuthPayload, AuthResult, UsersAuthResult } from "../server/dto/auth.dto";
-import { CreateProfile_Payload, ProfileResult } from "../server/dto/profile.dto";
+import { CreateProfile_Payload, ProfileResult, UpdateProfile_Payload } from "../server/dto/profile.dto";
 
 export interface AppServiceMap {
     auth: AuthService;
@@ -17,4 +17,6 @@ export interface ProfileService {
     createProfile(payload: CreateProfile_Payload): Promise<ProfileResult>;
 
     getDetail(payload: UserSession): Promise<ProfileResult>;
+
+    updateProfile(payload: UpdateProfile_Payload): Promise<ProfileResult>;
 }
