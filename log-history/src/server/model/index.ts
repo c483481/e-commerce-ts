2b5b1cd3376258e1c12ff9468dd8ac4e8d5e51d9
c483481 <charleskojansow@gmail.com>
2024-08-07 16:00:31 +0800

    add logs model

--- log-history/src/server/model/index.ts
+++ log-history/src/server/model/index.ts
@@ -1,11 +1,14 @@
 import { AuthHistory } from "./auth-history.model";
+import { Logs } from "./logs.model";
 
 export interface AppNoSqlModel {
     AuthHistory: typeof AuthHistory;
+    Logs: typeof Logs;
 }
 
 export function initNoSqlModels(): AppNoSqlModel {
     return {
         AuthHistory,
+        Logs,
     };
 }
