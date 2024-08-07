6a26e91c63a37013d4890449b41fca04e4e336a0
c483481 <charleskojansow@gmail.com>
2024-08-07 16:03:55 +0800

    add insert logs on repository

--- log-history/src/contract/repository.contract.ts
+++ log-history/src/contract/repository.contract.ts
@@ -1,9 +1,15 @@
 import { AuthHistoryAttribute, AuthHistoryCreationAttribute } from "../server/model/auth-history.model";
+import { LogsAttribute, LogsCreationAttribute } from "../server/model/logs.model";
 
 export interface AppRepositoryMap {
     authHistory: AuthHistoryRepository;
+    logs: LogsRepository;
 }
 
 export interface AuthHistoryRepository {
     insert(payload: AuthHistoryCreationAttribute): Promise<AuthHistoryAttribute>;
+}
+
+export interface LogsRepository {
+    insert(payload: LogsCreationAttribute): Promise<LogsAttribute>;
 }
