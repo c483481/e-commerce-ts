74946f9041182f28469e5b4b285c6e2ad8f9d6eb
c483481 <charleskojansow@gmail.com>
2024-08-13 19:26:11 +0800

    add insert category on repository

--- product/src/contract/repository.contract.ts
+++ product/src/contract/repository.contract.ts
@@ -1,1 +1,9 @@
-export interface AppRepositoryMap {}
+import { CategoryAttributes, CategoryCreationAttributes } from "../server/model/category.model";
+
+export interface AppRepositoryMap {
+    category: CategoryRepository;
+}
+
+export interface CategoryRepository {
+    insert(payload: CategoryCreationAttributes): Promise<CategoryAttributes>;
+}
--- product/src/server/repository/index.ts
+++ product/src/server/repository/index.ts
@@ -1,8 +1,11 @@
-import { AppRepositoryMap } from "../../contract/repository.contract";
+import { AppRepositoryMap, CategoryRepository } from "../../contract/repository.contract";
 import { AppDataSource } from "../../module/datasource.module";
 import { BaseRepository } from "./base.repository";
+import { SequelizeCategoryRepository } from "./category.repository";
 
 export class Repository implements AppRepositoryMap {
+    readonly category: CategoryRepository = new SequelizeCategoryRepository();
+
     init(datasource: AppDataSource) {
         Object.entries(this).forEach(([k, r]) => {
             if (r instanceof BaseRepository) {
